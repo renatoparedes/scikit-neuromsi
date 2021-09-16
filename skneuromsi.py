eeedab8938c92e3ca5f1771789debb9b4510571c
renatoparedes <paredesrenato92@gmail.com>
2021-09-16 12:32:12 -0500

    plot included

--- skneuromsi.py
+++ skneuromsi.py
@@ -1,6 +1,7 @@
 import abc
 import attr
 import numpy as np
+import matplotlib.pyplot as plt
 
 class MSIBrain(metaclass=abc.ABCMeta):
     
@@ -18,10 +19,13 @@ class MSIBrain(metaclass=abc.ABCMeta):
 
 
 @attr.s
-class ErnstBanks2002(MSIBrain):
+class AlaisBurr2004(MSIBrain):
     
     # Stimuli locations
     locs = attr.ib(default={"auditory": 0, "visual": 0})
+
+    # All possible locations
+    pos_locs = attr.ib(default=np.arange(-20,20,0.01))
     
     # Weights used to calculate the mean of the multimodal distribution
     weights = attr.ib(default={"auditory": 0.5, "visual": 0.5})
@@ -31,9 +35,6 @@ class ErnstBanks2002(MSIBrain):
 
     # Result
     _result = attr.ib(factory=dict)
-
-    # All possible locations
-    pos_locs = attr.ib(default=np.arange(-20,20,0.01))
 
     def __attrs_post_init__(self):
 
@@ -92,62 +93,26 @@ class ErnstBanks2002(MSIBrain):
     
     def __getitem__(self, modality):
         if modality == "auditory":
-            return self.auditory_modality
+            return self.auditory_modality    
         elif modality == "visual":
+            return self.visual_modality
+        elif modality == "multisensory":
             return self.visual_modality
         raise KeyError(modality)
         
     def response(self):
         return self._result["multisensory"]
     
-    
-ErnstBanks2002()
+    def plot(self):
+        for res in self._result:
+            plt.plot(self.pos_locs,self._result[res])
+        plt.ylabel("Probability density", size=12);
+        plt.xlabel("Position (degrees of visual angle)", size=12);
+        plt.legend(self._result.keys())
 
+    def run(self):
+        self.auditory_modality()
+        self.visual_modality()
+        self.multisensory_modality()
 
-# %% Cue combination
-
-# % We assume the visual and auditory probes can be displaced by up to 20
-# % degrees of visual angle in either direction from the centre of the
-# % display (as in Alais and Burr, 2004)
-# x = -20:0.01:20; % possible locations
-
-# x_v = 0;     % location for the visual stimulus
-# sigma_v = 4;  % variability of visual location estimates
-# x_a = 0;      % location for the auditory stimulus
-# sigma_a = 3;  % variability of auditory location estimates
-
-# % The distributions of visual and auditory location estimates are:
-# distr_v = (1/sqrt(2*pi*sigma_v^2))*exp(-1*(((x-x_v).^2)/(2*sigma_v^2))); % visual
-# distr_a = (1/sqrt(2*pi*sigma_a^2))*exp(-1*(((x-x_a).^2)/(2*sigma_a^2))); % auditory
-
-# % From Alais and Burr 2004, the weights used to calculation the mean of the
-# % multimodal distribution are:
-# w_v = sigma_a^2/(sigma_v^2+sigma_a^2);
-# w_a = sigma_v^2/(sigma_a^2+sigma_v^2);
-
-# % And the mean itself is:
-# x_va = w_v*x_v + w_a*x_a;
-
-# % From both Alais/Burr, 2004 and Ernst/Banks, 2002, the variability is:
-# sigma_va = sqrt((sigma_v^2*sigma_a^2)/(sigma_a^2+sigma_v^2));
-
-# % Substituting into the standard equation for a Gaussian, the distribution is:
-# distr_va = (1/sqrt(2*pi*sigma_va^2))*exp(-1*(((x-x_va).^2)/(2*sigma_va^2)));
-
-# % To check it has the correct form, we compare it to the product of the
-# % distributions:
-# distr_prod = (1/(2*pi*sigma_v*sigma_a))*exp(-1*(((x-x_v).^2)/(2*sigma_v^2))-1*(((x-x_a).^2)/(2*sigma_a^2))); % calculate product (proportional to combined distribution)
-# distr_prod = 100*distr_prod./sum(distr_prod); % normalize and rescale
-# prodForPlot = distr_prod(1:100:4001); % extract a few values to plot as scatter
-
-# % Plot all these distributions on the same set of axes
-# distributions = figure;
-# plot(x,distr_v,'b')
-# hold on 
-# plot(x,distr_a,'r')
-# plot(x,distr_va,'m')
-# scatter(-20:20,prodForPlot,'o','MarkerEdgeColor','k','MarkerFaceColor','g')
-# %title('Distributions of location estimates','FontSize',14)
-# ylabel('Probability density','FontSize',12)
-# xlabel('Position (degrees of visual angle)','FontSize',12)
-# legend('Visual','Auditory','Multimodal','Product of unimodals')
+AlaisBurr2004()
