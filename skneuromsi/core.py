2c8c484cdf7093cb2260324ec11e6e60af956b0d
renatoparedes <paredesrenato92@gmail.com>
2021-09-24 00:23:51 -0500

    attrs init bug fix

--- skneuromsi/core.py
+++ skneuromsi/core.py
@@ -23,62 +23,73 @@ class MSIBrain(metaclass=abc.ABCMeta):
 class AlaisBurr2004(MSIBrain):
 
     # Stimuli locations
-    locs = attr.ib(default={"auditory": 0, "visual": 0})
+    locs = attr.ib(factory=dict)
 
-    # All possible locations
-    pos_locs = attr.ib(default=np.arange(-20, 20, 0.01))
+    # All possible locations - Experiment setup
+    pos_locs = attr.ib()
+
+    @pos_locs.default
+    def _pos_locs_default(self):
+        return np.arange(-20, 20, 0.01)
 
     # Weights used to calculate the mean of the multimodal distribution
-    weights = attr.ib(default={"auditory": 0.5, "visual": 0.5})
+    weights = attr.ib(factory=dict)
 
     # Variability of stimulus estimates
-    e_sigmas = attr.ib(default={"auditory": 3, "visual": 4})
+    e_sigmas = attr.ib(factory=dict)
 
     # Result
     _result = attr.ib(factory=dict)
 
     def __attrs_post_init__(self):
 
+        # Stimuli setup
+        self.locs["auditory"] = 0
+        self.locs["visual"] = 0
+
+        loc_a = self.locs["auditory"]
+        loc_v = self.locs["visual"]
+
+        # Model setup
+        self.e_sigmas["auditory"] = 3
+        self.e_sigmas["visual"] = 4
+
+        sig_v = self.e_sigmas["auditory"]
+        sig_a = self.e_sigmas["visual"]
+
         # From Alais and Burr 2004, the weights used to calculation
         # the mean of the multimodal distribution are:
 
-        self.weights["auditory"] = self.e_sigmas["visual"] ** 2 / (
-            self.e_sigmas["auditory"] ** 2 + self.e_sigmas["visual"] ** 2
-        )
+        self.weights["auditory"] = sig_v ** 2 / (sig_a ** 2 + sig_v ** 2)
 
-        self.weights["visual"] = self.e_sigmas["auditory"] ** 2 / (
-            self.e_sigmas["visual"] ** 2 + self.e_sigmas["auditory"] ** 2
-        )
+        self.weights["visual"] = sig_a ** 2 / (sig_v ** 2 + sig_a ** 2)
+
+        w_a = self.weights["auditory"]
+        w_v = self.weights["visual"]
 
         # From both Alais/Burr, 2004 and Ernst/Banks, 2002,
         # the multisensory variability is:
         self.e_sigmas["multisensory"] = np.sqrt(
-            (self.e_sigmas["visual"] ** 2 * self.e_sigmas["auditory"] ** 2)
-            / (self.e_sigmas["auditory"] ** 2 + self.e_sigmas["visual"] ** 2)
+            (sig_v ** 2 * sig_a ** 2) / (sig_a ** 2 + sig_v ** 2)
         )
 
         # And the multisensory loc is:
-        self.locs["multisensory"] = (
-            self.weights["visual"] * self.locs["visual"]
-            + self.weights["auditory"] * self.locs["auditory"]
-        )
+        self.locs["multisensory"] = w_v * loc_v + w_a * loc_a
 
     def auditory_modality(self):
         """
         Computes auditory estimate
         """
 
-        if "auditiva" in self._result:
+        sig_a = self.e_sigmas["auditory"]
+        loc_a = self.locs["auditory"]
+        locs = self.pos_locs
+
+        if "auditory" in self._result:
             raise ValueError()
 
-        distr_a = (
-            1 / np.sqrt(2 * np.pi * self.e_sigmas["auditory"] ** 2)
-        ) * np.exp(
-            -1
-            * (
-                ((self.pos_locs - self.locs["auditory"]) ** 2)
-                / (2 * self.e_sigmas["auditory"] ** 2)
-            )
+        distr_a = (1 / np.sqrt(2 * np.pi * sig_a ** 2)) * np.exp(
+            -1 * (((locs - loc_a) ** 2) / (2 * sig_a ** 2))
         )
 
         self._result["auditory"] = distr_a
@@ -88,17 +99,15 @@ class AlaisBurr2004(MSIBrain):
         Computes visual estimate
         """
 
+        sig_v = self.e_sigmas["visual"]
+        loc_v = self.locs["visual"]
+        locs = self.pos_locs
+
         if "visual" in self._result:
             raise ValueError()
 
-        distr_v = (
-            1 / np.sqrt(2 * np.pi * self.e_sigmas["visual"] ** 2)
-        ) * np.exp(
-            -1
-            * (
-                ((self.pos_locs - self.locs["visual"]) ** 2)
-                / (2 * self.e_sigmas["visual"] ** 2)
-            )
+        distr_v = (1 / np.sqrt(2 * np.pi * sig_v ** 2)) * np.exp(
+            -1 * (((locs - loc_v) ** 2) / (2 * sig_v ** 2))
         )
 
         self._result["visual"] = distr_v
@@ -107,18 +116,15 @@ class AlaisBurr2004(MSIBrain):
         """
         Computes multisensory estimate
         """
+        sig_m = self.e_sigmas["multisensory"]
+        loc_m = self.locs["multisensory"]
+        locs = self.pos_locs
 
         if "multisensory" in self._result:
             raise ValueError()
 
-        distr_m = (
-            1 / np.sqrt(2 * np.pi * self.e_sigmas["multisensory"] ** 2)
-        ) * np.exp(
-            -1
-            * (
-                ((self.pos_locs - self.locs["multisensory"]) ** 2)
-                / (2 * self.e_sigmas["multisensory"] ** 2)
-            )
+        distr_m = (1 / np.sqrt(2 * np.pi * sig_m ** 2)) * np.exp(
+            -1 * (((locs - loc_m) ** 2) / (2 * sig_m ** 2))
         )
 
         self._result["multisensory"] = distr_m
