708f5e33c23fd3f3d2dc00ced8a53c3ea9274162
renatoparedes <paredesrenato92@gmail.com>
2021-10-22 15:38:30 -0500

    ErnstBanks fix

--- skneuromsi/ernst_banks2002.py
+++ skneuromsi/ernst_banks2002.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-import skneuromsi as core
+from . import core
 
 
 def haptic_estimator(haptic_sigma, posible_locations, haptic_location):
@@ -54,7 +54,7 @@ class ErnstBanks2002:
 
     # hiper parameters
     posible_locations = core.hparameter(
-        factory=lambda: np.arange(-20, 20, 0.01)
+        factory=lambda: np.arange(-20, 20, 0.01) # TODO fix locations
     )
     haptic_sigma = core.hparameter(default=4)
     visual_sigma = core.hparameter(default=1)
@@ -64,16 +64,16 @@ class ErnstBanks2002:
 
     @haptic_weight.default
     def _haptic_weight_default(self):
-        return (1 / self.visual_sigma ** 2) / (
-            (1 / self.haptic_sigma ** 2) + (1 / self.visual_sigma ** 2)
+        return self.visual_sigma ** 2 / (
+            self.haptic_sigma ** 2 + self.visual_sigma ** 2
         )
 
     visual_weight = core.internal()
 
     @visual_weight.default
     def _visual_weights_default(self):
-        return (1 / self.haptic_sigma ** 2) / (
-            (1 / self.visual_sigma ** 2) + (1 / self.haptic_sigma ** 2)
+        return self.haptic_sigma ** 2 / (
+            self.visual_sigma ** 2 + self.haptic_sigma ** 2
         )
 
     multisensory_sigma = core.internal()
