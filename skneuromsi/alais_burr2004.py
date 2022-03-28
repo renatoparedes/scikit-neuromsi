5da9f194df3987264acb34e4380f8edb696c44ae
renatoparedes <paredesrenato92@gmail.com>
2022-03-27 23:12:29 -0500

    AlaisBurr model update

---
+++ skneuromsi/alais_burr2004.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Scikit-NeuroMSI Project (https://github.com/renatoparedes/scikit-neuromsi).
+# Copyright (c) 2021-2022, Renato Paredes; Cabral, Juan
+# License: BSD 3-Clause
+# Full Text:
+#     https://github.com/renatoparedes/scikit-neuromsi/blob/main/LICENSE.txt
+
+"""
+Implementation of multisensory integration neurocomputational models in Python.
+"""
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import numpy as np
+
+from .core import SKNMSIMethodABC
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+
+class AlaisBurr2004(SKNMSIMethodABC):
+    """Zaraza.
+
+
+    References
+    ----------
+    :cite:p:`cuppini2017biologically`
+
+    """
+
+    _model_name = "AlaisBurr2004"
+    _run_input = [
+        {"target": "auditory_position", "template": "${mode0}_position"},
+        {"target": "visual_position", "template": "${mode1}_position"},
+    ]
+
+    _run_output = [
+        {"target": "auditory", "template": "${mode0}"},
+        {"target": "visual", "template": "${mode1}"},
+    ]
+
+    def __init__(
+        self,
+        *,
+        possible_locations=np.arange(-20, 20, 0.01),
+        auditory_sigma=3.0,
+        visual_sigma=3.0,
+        mode0="auditory",
+        mode1="visual",
+    ):
+
+        self._possible_locations = possible_locations
+
+        self._auditory_sigma = auditory_sigma
+        self._visual_sigma = visual_sigma
+
+        self._auditory_weight = np.square(visual_sigma) / (
+            np.square(auditory_sigma) + np.square(visual_sigma)
+        )
+
+        self._visual_weight = np.square(auditory_sigma) / (
+            np.square(visual_sigma) + np.square(auditory_sigma)
+        )
+
+        self._multisensory_sigma = np.sqrt(
+            (np.square(visual_sigma) * np.square(auditory_sigma))
+            / (np.square(auditory_sigma) + np.square(visual_sigma))
+        )
+
+        self._mode0 = mode0
+        self._mode1 = mode1
+
+    # PROPERTY ================================================================
+
+    @property
+    def possible_locations(self):
+        return self._possible_locations
+
+    @property
+    def auditory_sigma(self):
+        return self._auditory_sigma
+
+    @property
+    def visual_sigma(self):
+        return self._visual_sigma
+
+    @property
+    def auditory_weight(self):
+        return self._auditory_weight
+
+    @property
+    def visual_weight(self):
+        return self._visual_weight
+
+    @property
+    def multisensory_sigma(self):
+        return self._multisensory_sigma
+
+    @property
+    def mode0(self):
+        return self._mode0
+
+    @property
+    def mode1(self):
+        return self._mode1
+
+    # Model methods
+    def unisensory_estimator(self, unisensory_sigma, unisensory_position):
+
+        sigma = unisensory_sigma
+        location = unisensory_position
+        plocations = self.possible_locations
+
+        unisensory_estimate = (
+            1 / np.sqrt(2 * np.pi * np.square(sigma))
+        ) * np.exp(
+            -1 * (((plocations - location) ** 2) / (2 * np.square(sigma)))
+        )
+
+        return unisensory_estimate
+
+    def multisensory_estimator(
+        self,
+        unisensory_position_a,
+        unisensory_position_b,
+        unisensory_weight_a,
+        unisensory_weight_b,
+        multisensory_sigma,
+    ):
+
+        sigma = multisensory_sigma
+
+        location = (
+            unisensory_weight_a * unisensory_position_a
+            + unisensory_weight_b * unisensory_position_b
+        )
+        plocations = self.possible_locations
+
+        multisensory_estimate = (
+            1 / np.sqrt(2 * np.pi * np.square(sigma))
+        ) * np.exp(
+            -1 * (((plocations - location) ** 2) / (2 * np.square(sigma)))
+        )
+
+        return multisensory_estimate
+
+    # Model run
+    def run(
+        self,
+        auditory_position,
+        visual_position,
+    ):
+
+        auditory_estimate = self.unisensory_estimator(
+            self.auditory_sigma, auditory_position
+        )
+        visual_estimate = self.unisensory_estimator(
+            self.visual_sigma, visual_position
+        )
+
+        auditory_weight = self.auditory_weight
+
+        visual_weight = self.visual_weight
+
+        multisensory_sigma = self.multisensory_sigma
+
+        multisensory_estimate = self.multisensory_estimator(
+            auditory_position,
+            visual_position,
+            auditory_weight,
+            visual_weight,
+            multisensory_sigma,
+        )
+
+        return {
+            "auditory": auditory_estimate,
+            "visual": visual_estimate,
+            "multi": multisensory_estimate,
+        }
