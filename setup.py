c46280c01f0806f42a2f0238a25b51a2d61db3c5
juanbc <jbcabral@unc.edu.ar>
2022-05-16 00:59:45 -0300

    xarray to setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from setuptools import setup
 
 PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
 
-REQUIREMENTS = ["numpy", "brain-py", "jaxlib", "bidict", "seaborn"]
+REQUIREMENTS = ["numpy", "brain-py", "jaxlib", "bidict", "seaborn", "xarray"]
 
 with open(PATH / "skneuromsi" / "__init__.py") as fp:
     for line in fp.readlines():
