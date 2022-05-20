c84642eb2289a47f17fd442f4faf16077dcdbeed
juanbc <jbcabral@unc.edu.ar>
2022-05-19 23:48:32 -0300

    plot with only one time or positions works

--- skneuromsi/core/plot.py
+++ skneuromsi/core/plot.py
@@ -27,6 +27,8 @@ import seaborn as sns
 
 import matplotlib.pyplot as plt
 
+import xarray as xr
+
 from ..utils import AccessorABC
 
 # =============================================================================
@@ -67,41 +69,65 @@ class ResultPlotter(AccessorABC):
 
     """
 
-    _default_kind = "pline"
+    _default_kind = "linep"
 
     def __init__(self, result):
         self._result = result
 
     # LINE ====================================================================
-    def _resolve_axis(self, positions_coordinates_number, ax):
+    def _resolve_axis(self, ax):
 
         if ax is None:
-            fig, ax = plt.subplots(
-                1, positions_coordinates_number, sharey=True
-            )
+            coords_number = len(self._result.pcoords_)
+
+            fig, ax = plt.subplots(1, coords_number, sharey=True)
 
             size_x, size_y = fig.get_size_inches()
-            fig.set_size_inches(size_x * positions_coordinates_number, size_y)
+            fig.set_size_inches(size_x * coords_number, size_y)
 
         if not isinstance(ax, Iterable):
             ax = np.array([ax])
 
         return ax
 
+    def _resolve_fix_dimension(self, dim, dim_value, xa):
+        if dim_value in HEURISTICS:
+            dim_value = HEURISTICS[dim_value]
+        if callable(dim_value):
+            dim_value = dim_value(dim, xa)
+        return dim_value
+
+    def _complete_dimension(self, xa, dim, n, scalar_dim=True):
+        # the original array must be in the final data
+        completed = [xa]
+
+        zxa = xa.copy()
+        zxa.values = np.zeros_like(zxa.values)
+
+        for idx in np.arange(-n, n + 1):
+            if idx == 0:
+                continue
+
+            nxa = zxa.copy()
+            nxa[dim] = idx if scalar_dim else [idx]
+            completed.append(nxa)
+
+        return xr.combine_nested(completed, dim)
+
     def linep(self, time="mean_max_value", **kwargs):
-
-        coords = self._result.pcoords_
-
-        ax = self._resolve_axis(len(coords), kwargs.pop("ax", None))
 
         xa = self._result.to_xarray()
 
-        if time in HEURISTICS:
-            timeheu = HEURISTICS[time]
-            time = timeheu("times", xa)
+        time = self._resolve_fix_dimension("times", time, xa)
+        axes = self._resolve_axis(kwargs.pop("ax", None))
+        has_single_position = len(self._result.positions_) == 1
 
+        if has_single_position:
+            xa = self._complete_dimension(xa, "positions", 25, scalar_dim=True)
 
-        for coord, ax in zip(coords, ax):
+        kwargs.setdefault("alpha", 0.75)
+
+        for coord, ax in zip(self._result.pcoords_, axes):
 
             df = xa.sel(positions_coordinates=coord, times=time).to_dataframe()
 
@@ -114,34 +140,41 @@ class ResultPlotter(AccessorABC):
                 **kwargs,
             )
 
-        ax.set_title(f"Time {time}")
+        model_name = self._result.mname
+        ax.set_title(f"{model_name} - Time {time}")
         ax.legend()
 
         return ax
 
     def linet(self, position="mean_max_value", **kwargs):
 
-        coords = self._result.pcoords_
-
-        ax = self._resolve_axis(len(coords), kwargs.pop("ax", None))
-
         xa = self._result.to_xarray()
+        position = self._resolve_fix_dimension("positions", position, xa)
+        axes = self._resolve_axis(kwargs.pop("ax", None))
+        has_single_time = len(self._result.times_) == 1
 
-        if position in HEURISTICS:
-            posheu = HEURISTICS[position]
-            position = posheu("positions", xa)
+        if has_single_time:
+            xa = self._complete_dimension(xa, "times", 25, scalar_dim=False)
 
-        for coord, ax in zip(coords, ax):
+        kwargs.setdefault("alpha", 0.75)
+
+        for coord, ax in zip(self._result.pcoords_, axes):
 
             df = xa.sel(
                 positions_coordinates=coord, positions=position
             ).to_dataframe()
 
             sns.lineplot(
-                x="times", y="values", hue="modes", data=df, ax=ax, **kwargs
+                x="times",
+                y="values",
+                hue="modes",
+                data=df,
+                ax=ax,
+                **kwargs,
             )
 
-        ax.set_title(f"Position {position}")
+        model_name = self._result.mname
+        ax.set_title(f"{model_name} - Position {position}")
         ax.legend()
 
         return ax
