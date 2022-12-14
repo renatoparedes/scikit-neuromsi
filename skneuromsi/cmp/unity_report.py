f737aaaaf8e4f4501c27028421e38bc9a24b57d2
juanbc <jbcabral@unc.edu.ar>
2022-12-14 16:21:49 -0300

    unity report works!

--- skneuromsi/cmp/unity_report.py
+++ skneuromsi/cmp/unity_report.py
@@ -28,7 +28,7 @@ import numpy as np
 
 import pandas as pd
 
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 # =============================================================================
 # CONSTANTS
@@ -36,6 +36,7 @@ from tqdm import tqdm
 
 
 DEFAULT_RANGE = 90 + np.arange(0, 20, 2)
+
 
 # =============================================================================
 # CLASS
@@ -51,6 +52,7 @@ class UnityReport:
         repeat=100,
         n_jobs=1,
         target_rx=re.compile(r".*_position$"),
+        seed=None,
         progress_cls=tqdm,
     ):
         self._model = model
@@ -60,6 +62,7 @@ class UnityReport:
         self._repeat = int(repeat)
         self._n_jobs = int(n_jobs)
         self._target_rx = re.compile(target_rx)
+        self._random = np.random.default_rng(seed)
         self._progress_cls = progress_cls
 
         run_signature = inspect.signature(model.run)
@@ -100,8 +103,9 @@ class UnityReport:
         return self._run_params_targets
 
     def _run_kwargs_combinations(self, run_kws):
-        def combs_gen():
+        iinfo = np.iinfo(int)
 
+        def combs_gen():
             # combine all targets with all possible values
             tgt_x_range = it.chain(
                 it.product([tgt], self._range)
@@ -114,7 +118,8 @@ class UnityReport:
 
                 # repeat the combination the number of times
                 for _ in range(self._repeat):
-                    yield comb_as_kws.copy()
+                    seed = self._random.integers(low=0, high=iinfo.max)
+                    yield comb_as_kws.copy(), seed
 
         combs_size = (
             (len(self._run_params_targets) * len(self._range))
@@ -122,8 +127,11 @@ class UnityReport:
 
         return combs_gen(), combs_size
 
-    def _run_report(self, run_kws):
+    def _run_report(self, run_kws, seed):
+
         model = self._model
+        model.set_random(np.random.default_rng(seed))
+
         response = model.run(**run_kws)
         row = {
             k: v for k, v in run_kws.items() if k in self._run_params_targets
@@ -147,11 +155,12 @@ class UnityReport:
             )
         # we execute the first iteration synchronous so if some configuration
         # fails we can catch it here
-        first_response = self._run_report(next(rkw_combs))
+        rkw, rkw_seed = next(rkw_combs)
+        first_response = self._run_report(rkw, rkw_seed)
 
         with joblib.Parallel(n_jobs=self._n_jobs) as P:
             drun = joblib.delayed(self._run_report)
-            responses = P(drun(rkw) for rkw in rkw_combs)
+            responses = P(drun(rkw, rkw_seed) for rkw, rkw_seed in rkw_combs)
 
         responses.insert(0, first_response)
         return pd.DataFrame(responses)
