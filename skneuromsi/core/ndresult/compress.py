38dd1a6f87bafb6800ff3bd115021fb974a07402
juanbc <jbcabral@unc.edu.ar>
2024-07-21 21:12:57 -0300

    start porting to ndresult compressed into their own file

---
+++ skneuromsi/core/ndresult/compress.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Scikit-NeuroMSI Project (https://github.com/renatoparedes/scikit-neuromsi).
+# Copyright (c) 2021-2022, Renato Paredes; Cabral, Juan
+# License: BSD 3-Clause
+# Full Text:
+#     https://github.com/renatoparedes/scikit-neuromsi/blob/main/LICENSE.txt
+
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""Compress ndresult data and store it in a compressed format.
+
+"""
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import copy
+import dataclasses as dclss
+import io
+
+import joblib
+
+import numpy as np
+
+import pandas as pd
+
+import xarray as xa
+
+from ..utils import ddtype_tools
+from .result import NDResult
+
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+#: Default compression parameters. (see joblib.dump)
+DEFAULT_COMPRESSION_PARAMS = ("zlib", 9)
+
+#: Types that can be compressed.
+COMPRESS_TYPES = (np.ndarray, xa.DataArray, pd.DataFrame, pd.Series)
+
+
+# =============================================================================
+# CLASSES
+# =============================================================================
+
+
+@dclss.dataclass(slots=True, frozen=True, repr=False)
+class CompressedNDResult:
+    """
+    A dataclass for storing compressed NDResults.
+
+    Attributes
+    ----------
+    cls : type
+        The class type of the original data.
+    data : object
+        The compressed data.
+    """
+
+    data: dict
+    original_memory_usage: ddtype_tools.MemoryUsage
+    compressed_memory_usage: ddtype_tools.MemoryUsage
+    compressed_extra_keys: frozenset
+
+    @property
+    def compression(self):
+        """Return the compression ratio."""
+        return (
+            self.compressed_memory_usage.size / self.original_memory_usage.size
+        )
+
+    def __repr__(self):
+        """Return a string representation of the CompressedNDResult object."""
+        cls_name = type(self).__name__
+
+        compressed_size = self.compressed_memory_usage.hsize
+        compression_percentage = self.compression * 100
+
+        return (
+            f"<{cls_name} {compressed_size!r} ({compression_percentage:.2f}%)>"
+        )
+
+
+# COMPRESSION =================================================================
+
+
+def _compress_object(obj, compress_params):
+    stream = io.BytesIO()
+    joblib.dump(obj, stream, compress=compress_params)
+    return stream.getvalue()
+
+
+def compress_ndresult(ndresult, compress_params=DEFAULT_COMPRESSION_PARAMS):
+
+    if not isinstance(ndresult, NDResult):
+        raise TypeError("Not an NDResult")
+
+    ndresult_dict = ndresult.to_dict()
+
+    # all the parts without compression
+    compressed_ndresult_dict = {
+        k: v for k, v in ndresult_dict.items() if k not in ("nddata", "extra")
+    }
+
+    # compress nddata
+    compressed_ndresult_dict["nddata"] = _compress_object(
+        ndresult_dict["nddata"], compress_params=compress_params
+    )
+
+    # compress extra
+    compressed_extra = {}
+    compressed_extra_keys = set()
+    for k, v in ndresult_dict["extra"].items():
+
+        if isinstance(v, COMPRESS_TYPES):
+            v = _compress_object(v, compress_params=compress_params)
+            compressed_extra_keys.add(k)
+
+        compressed_extra[k] = v
+
+    compressed_ndresult_dict["extra"] = compressed_extra
+
+    original_memory_usage = ddtype_tools.memory_usage(ndresult)
+    compressed_memory_usage = ddtype_tools.memory_usage(
+        compressed_ndresult_dict
+    )
+
+    return CompressedNDResult(
+        data=compressed_ndresult_dict,
+        compressed_extra_keys=frozenset(compressed_extra_keys),
+        original_memory_usage=original_memory_usage,
+        compressed_memory_usage=compressed_memory_usage,
+    )
+
+
+# DECOMPRESSION ===============================================================
+
+
+def _decompress_object(obj):
+    stream = io.BytesIO(obj)
+    return joblib.load(stream)
+
+
+def decompress_ndresult(compressed_ndresult):
+    """Decompress an NDResult object.
+
+    Parameters
+    ----------
+    compressed_ndresult : CompressedNDResult
+        The compressed NDResult object to decompress.
+
+    Returns
+    -------
+    NDResult
+        The decompressed NDResult object.
+
+    """
+
+    if not isinstance(compressed_ndresult, CompressedNDResult):
+        raise TypeError("Not a compressed NDResult")
+
+    ndresult_dict = copy.deepcopy(compressed_ndresult.data)
+
+    # uncompress data
+    ndresult_dict["nddata"] = _decompress_object(
+        ndresult_dict["nddata"]
+    )
+
+    # uncompress extra
+    extra = ndresult_dict["extra"]
+    for k in compress_ndresult.compressed_extra_keys:
+        extra[k] = _decompress_object(extra[k])
+
+    return NDResult(**ndresult_dict)
