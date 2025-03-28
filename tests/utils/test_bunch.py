827738452dbec0ed95be8c7b8e567186924bde32
juanbc <jbcabral@unc.edu.ar>
2025-03-28 11:31:08 -0300

    header updated

--- tests/utils/test_bunch.py
+++ tests/utils/test_bunch.py
@@ -3,7 +3,7 @@
 
 # This file is part of the
 #   Scikit-NeuroMSI Project (https://github.com/renatoparedes/scikit-neuromsi).
-# Copyright (c) 2021-2022, Renato Paredes; Cabral, Juan
+# Copyright (c) 2021-2025, Renato Paredes; Cabral, Juan
 # License: BSD 3-Clause
 # Full Text:
 #     https://github.com/renatoparedes/scikit-neuromsi/blob/main/LICENSE.txt
